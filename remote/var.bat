c89a427d4b2f68c07d3c7e9b60d59325907d0336
wellsilver <67511181+wellsilver@users.noreply.github.com>
2022-01-08 11:51:46 -0500

    Update var.bat

--- remote/var.bat
+++ remote/var.bat
@@ -4,6 +4,6 @@ REM why? This is because the free hoster limits bandwith so I need to download t
 REM original jars [no prefix]
 
 REM paper (may be outdated lol) [P before begining]
-if %i%==P1.18 bitsadmin /transfer "Download Paper1.18#140" /download /priority foreground /api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar
-if %i%==P1.18.1 bitsadmin /transfer "Download Paper1.18#140" /download /priority foreground /api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar
-REM spigot (may be outdated lol)[s before begginging]
+if %i%==P1.18 bitsadmin /transfer "Download Paper1.18#140" /download /priority foreground papermc.io/api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar %APPDATA%/mcserversimplified/prim/%i%.jar
+if %i%==P1.18.1 bitsadmin /transfer "Download Paper1.18#140" /download /priority foreground papermc.io/api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar %APPDATA%/mcserversimplified/prim/%i%.jar
+
