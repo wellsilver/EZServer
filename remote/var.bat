c51d49b964f4a24e4dfbbbd09b8dd38f81f5627f
wellsilver <wellsilver2016@gmail.com>
2022-01-20 15:36:54 -0500

    Update var.bat

--- remote/var.bat
+++ remote/var.bat
@@ -1,6 +1,6 @@
 REM why? This is because the free hoster limits bandwith so I need to download the files from their source
 
 REM original jars [no prefix]
-
+if %i%==1.18 bitsadmin /transfer "Download 1.18" /download /priority foreground https://launcher.mojang.com/v1/objects/125e5adf40c659fd3bce3e66e67a16bb49ecc1b9/server.jar %APPDATA%/mcserversimplified/prim/jar.jar
 REM paper (may be outdated lol) [P before begining]
-if %i%==P1.18 bitsadmin /transfer "Download 1.18" /download /priority foreground https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar %APPDATA%/mcserversimplified/prim/jar.jar
+if %i%==P1.18 bitsadmin /transfer "Download Laper 1.18#140" /download /priority foreground https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/140/downloads/paper-1.18.1-140.jar %APPDATA%/mcserversimplified/prim/jar.jar
