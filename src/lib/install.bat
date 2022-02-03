63637253f98e0c8c742efd327ffc929d1f4dbaf2
wellsilver <wellsilver2016@gmail.com>
2022-02-03 18:49:46 -0500

    Update install.bat

--- src/lib/install.bat
+++ src/lib/install.bat
@@ -9,3 +9,11 @@ echo The files will download at:
 echo %APPDATA%/EZServer5
 
 :installoutdated
+cls
+echo Welcome to the install wizard!
+echo The files that will be deleted:
+echo %APPDATA%/mcserversimplified
+echo the files that will be downloaded:
+echo %APPDATA%/EZServer5
+echo When deleting files you may be prompted.
+echo You may read if you like but please accept.
