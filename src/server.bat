c3feb5c9bd4096d34331a63a254a5652a6265a99
wellsilver <wellsilver2016@gmail.com>
2022-02-04 08:35:38 -0500

    Update server.bat

--- src/server.bat
+++ src/server.bat
@@ -1,10 +1,11 @@
 @echo off
-::first check if its outdated
+::first check if its outdated or uninstalled
 if exist %APPDATA%/mcserversimplified goto outdated
-
+if not exist %APPDATA%/EZServer5 goto install.bat
 ::version
 set /a launcherv=Version-5-BETA
 ::generate job ID for bitsadmin
+::for anyone copying code %random% is very predictable so is unsuitable for security
 set /a bitsidv=%random%
 
 :home
@@ -20,7 +21,7 @@ echo In any menu you can type "back" to go back.
 set /p e=$ 
 
 if %e%==list goto list
-if %e%==doc goto doc
+:: if %e%==doc call doc.bat
 if %e%==start goto start
 if %e%==back goto easteregg
 
@@ -29,14 +30,38 @@ goto home
 :start
 cls
 echo Welcome to MCServer setup
-echo To go with your previous setup just type "start" again.
+echo To go with your current setup just type "start" again.
 echo To add plugins type "plugin"
-echo To download the verson type "ver"
+echo To download/change the verson type "ver"
 echo Type "list" to list available versions
 :: echo to configure ram, port, etc, type "config"
 set /p r=$ 
+if %r%==start goto s1
+if %r%==plugin goto pluginz
+if %r%==ver goto verdown
+if %r%==back goto home
 
 goto start
+
+
+:verdown
+if exist %APPDATA%/EZServer5/lol.jar
+cls
+echo.
+echo Type the version to download:
+echo.
+set /p h=%e%
+::stuff here to download verzionz.bat and run it
+goto start
+
+:s1
+echo Version: %launcherv%
+echo Minecraft version: %h%
+echo IP: %?%
+echo.
+java -jar lol.jar --nogui
+if %l%==1 goto s1
+if %l%==2 goto start
 
 
 :easteregg
