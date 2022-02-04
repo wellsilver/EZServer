1b9e308531a9980dd709ea749e22d0e53e186353
wellsilver <wellsilver2016@gmail.com>
2022-02-04 18:23:16 -0500

    Update exe.bat

--- src/exe.bat
+++ src/exe.bat
@@ -1,10 +1,12 @@
-:: This is the EXE, it downloads and runs server.bat
+
 @echo off
+:: This is the EXE, it downloads and runs server.bat
+:h
 set /a launcherv=5
 if exist %APPDATA%/mcserversimplified goto installoutdated
 if not exist %APPDATA%/EZServer5 goto install
-if not exist %APPDATA%/EZServer5/server.bat goto corrupted
 call %APPDATA%/EZServer5/server.bat
+if %ERRORLEVEL%==1 goto corrupted
 goto eof
 
 :install
@@ -16,11 +18,13 @@ echo %APPDATA%/EZServer5
 echo.
 pause
 mkdir %APPDATA%\EZServer5
+bitsadmin /transfer "Download" /download /priority foreground https://wellsilver.github.io/a/EZServer/server.bat %APPDATA%/EZServer5/server.bat
 mkdir %APPDATA%\EZServer5\bin
 mkdir %APPDATA%\EZServer5\plugins
 mkdir %APPDATA%\EZServer5\configs
 mkdir %APPDATA%\EZServer5\injar
 mkdir %APPDATA%\EZServer5\serv
+goto h
 :corrupted
 cls
 echo oops! looks like the files are corrupted
@@ -29,6 +33,9 @@ echo and go back to the installation prompt
 echo.
 pause
 if exist %APPDATA%/EZServer5 goto corrupted1
+goto install
+:corrupted1
+del %APPDATA%/EZServer5
 goto install
 :installoutdated
 cls
@@ -46,8 +53,10 @@ echo When deleting files you may be prompted.
 pause
 del %APPDATA%\mcserversimplified
 mkdir %APPDATA%\EZServer5
+bitsadmin /transfer "Download" /download /priority foreground https://wellsilver.github.io/a/EZServer/server.bat %APPDATA%/EZServer5/server.bat
 mkdir %APPDATA%\EZServer5\bin
 mkdir %APPDATA%\EZServer5\plugins
 mkdir %APPDATA%\EZServer5\configs
 mkdir %APPDATA%\EZServer5\injar
 mkdir %APPDATA%\EZServer5\serv
+goto h
