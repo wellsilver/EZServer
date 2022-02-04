52cf193f293846afd8f1a03401ebd34d77418b07
wellsilver <wellsilver2016@gmail.com>
2022-02-03 19:42:14 -0500

    Update install.bat

--- src/lib/install.bat
+++ src/lib/install.bat
@@ -6,14 +6,28 @@ if exist %APPDATA%/mcserversimplified goto installoutdated
 cls
 echo Welcome to the install wizard!
 echo The files will download at:
+echo.
 echo %APPDATA%/EZServer5
+echo.
+pause
 
 :installoutdated
 cls
 echo Welcome to the install wizard!
+echo.
 echo The files that will be deleted:
-echo %APPDATA%/mcserversimplified
+echo.
+echo %APPDATA%\mcserversimplified
+echo.
 echo the files that will be downloaded:
-echo %APPDATA%/EZServer5
+echo.
+echo %APPDATA%\EZServer5
+echo.
 echo When deleting files you may be prompted.
-echo You may read if you like but please accept.
+pause
+del %APPDATA%\mcserversimplified
+mkdir %APPDATA%\EZServer5
+mkdir %APPDATA%\EZServer5\bin
+mkdir %APPDATA%\EZServer5\plugins
+mkdir %APPDATA%\EZServer5\configs
+mkdir %APPDATA%\EZServer5\injar
