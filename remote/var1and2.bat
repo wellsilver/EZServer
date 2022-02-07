2c494e6fe3e4b8b61f9218c453af65ceebbd122b
wellsilver <wellsilver2016@gmail.com>
2022-02-07 18:32:22 -0500

    Update var1and2.bat

--- remote/var1and2.bat
+++ remote/var1and2.bat
@@ -1,9 +1,11 @@
 @echo off
-:a
 cls
 echo A stable version has been released-
-echo Goto releases page?
+echo N Will bring you to the original
+echo Y Will bring you to the github releases page
+echo Keep in mind pre 1.0(internally dubbed "5")versions are no longer supported.
 set /p e=Y/N: 
-if %e%==Y start https://github.com/wellsilver/EZServer/releases
-if %e%==N goto a
-goto a
+if %e%==N goto b
+start https://github.com/wellsilver/EZServer/releases
+:b
+::original here
